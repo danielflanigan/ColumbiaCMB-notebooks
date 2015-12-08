f2b7e8a6b687c2fe9c61b7159ffdc352d0ab3446
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2015-12-08 18:03:48 -0500

    display.py and gaussmeter

--- library/display.py
+++ library/display.py
@@ -1,10 +1,20 @@
-try:
-    from ipywidgets import HTML
-except ImportError:
-    from IPython.display import HTML
+from IPython.core import display
 
 
+def hide_code(button=False):
+    display.display_html('<script>jQuery(function() {if (jQuery("body.notebook_app").length == 0) { jQuery('
+                   '".input_area").toggle(); jQuery(".prompt").toggle();}});</script>', raw=True)
+    if button:
+        display.display_html('''<button onclick="jQuery('.input_area').toggle(); jQuery('.prompt').toggle();">Toggle
+        code</button>''', raw=True)
+
+
+# Deprecated
 def toggle_input_code():
+    try:
+        from ipywidgets import HTML
+    except ImportError:
+        from IPython.display import HTML
     return HTML('''<script>
         code_show=true;
         function code_toggle() {
@@ -18,4 +28,6 @@ def toggle_input_code():
         $( document ).ready(code_toggle);
         </script>
         <form action="javascript:code_toggle()"><input type="submit" value="Hide/show the input code."></form>''')
+
+
 
